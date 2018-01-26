472e10152018a6817a3040e05c26ddef2aef1702
Wyrobek Andreas <andreas.wyrobek@acp.de>
2018-01-26 14:03:07 +0100

    add s3 support

--- automatic_backup/models/models.py
+++ automatic_backup/models/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, service
 from enum import Enum
 import boto3
 import botocore
 import os
-import re
+import datetime
 
 
 class BackupTypes(Enum):
@@ -19,11 +19,14 @@ class Configuration(models.Model):
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
 
-    success_mail = fields.Char("If successful, mail to")
-    error_mail = fields.Char("If error, mail to")
+    success_mail = fields.Many2one('res.users', ondelete='set null')
+    error_mail = fields.Many2one('res.users', ondelete='set null')
 
     backup_type = fields.Selection([BackupTypes.s3.value], required=True)
     upload_path = fields.Char("Path to upload", required=True)
+    last_backup = fields.Datetime(readonly=True)
+    last_message = fields.Char(readonly=True)
+    last_path = fields.Char(readonly=True)
 
     show_s3 = fields.Boolean(compute='set_show_s3', store=False)
     s3_access_key_id = fields.Char("Access Key")
@@ -62,25 +65,37 @@ class Configuration(models.Model):
     @api.multi
     def btn_action_backup(self):
         self.ensure_one()
-        if self.backup_type == BackupTypes.s3.value[0]:
-            self._backup_on_s3()
+        try:
+            if self.backup_type == BackupTypes.s3.value[0]:
+                self._backup_on_s3()
+                self.send_email('')
+        except Exception as err:
+            self.set_last_fields(err.args[0])
+            self.message_post(err.args[0])
+            self.send_email(err.args[0], success=False)
+
+    def send_email(self, message, success=True):
+        if ((success and self.success_mail.id is not False) or (not success and self.success_mail.id is not False)):
+            template_name = 'backup_configuration_success_template' if success else 'backup_configuration_error_template'
+            template = self.env.ref('automatic_backup.'+template_name)
+            self.env['mail.template'].browse(template.id).send_mail(self.id, force_send=True)
 
     def _backup_on_s3(self):
         if self.s3_access_key_id is False or self.s3_secret_access_key is False:
             raise exceptions.MissingError(
-                "You need to add a AccessKey and a SecretAccessKey!")
+                "AWS S3: You need to add a AccessKey and a SecretAccessKey!")
         if self.s3_bucket_name is False:
             raise exceptions.MissingError(
-                "You need to add a BucketName!")
+                "AWS S3: You need to add a BucketName!")
 
         client = boto3.client(
             's3',
             aws_access_key_id=self.s3_access_key_id,
             aws_secret_access_key=self.s3_secret_access_key
         )
-        filename = 'test.zip'
+        filename, content = self.get_backup()
         path = os.path.join(self.upload_path, filename)
-        content = 'test'
+
         try:
             client.put_object(
                 Bucket=self.s3_bucket_name,
@@ -89,5 +104,24 @@ class Configuration(models.Model):
             )
         except botocore.exceptions.ClientError as client_err:
             raise exceptions.ValidationError(
-                client_err.response['Error']['Message'])
-        self.message_post("No Error during upload. You can find the backup under the bucket {0} with the name {1}".format(self.s3_bucket_name, path))
+                "AWS S3: " + client_err.response['Error']['Message'])
+        else:
+            message = ("AWS S3: No Error during upload. You can "
+                      "find the backup under the bucket {0} with the "
+                      "name {1}".format(self.s3_bucket_name, path))
+            self.set_last_fields(message, path=path)
+            self.message_post(message)
+
+    def get_backup(self, dbname=None, backup_format='zip'):
+        if dbname is None:
+            dbname = self._cr.dbname
+        ts = datetime.datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = "%s_%s.%s" % (dbname, ts, backup_format)
+        dump_stream = service.db.dump_db(dbname, None, backup_format)
+        return filename, dump_stream
+
+    def set_last_fields(self, message, path=None):
+        if path is not None:
+            self.last_path = path
+            self.last_backup = datetime.datetime.now()
+        self.last_message = message
