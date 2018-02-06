98d04fdcfd1602195dabe879f6eee109f8e36c60
Andreas Wyrobek <andreas.wyrobek95@gmail.com>
2018-02-06 23:12:52 +0100

    add S3 support add Dropbox support Google Drive and owncloud/nextcloud dont work creates schedule job Bug found: scheduletime dont update in backup-view when changed in ir.cron add short readme

---
+++ README.md
@@ -0,0 +1,56 @@
+Odoo 11 - Automatic Backup to whatever
+===============
+
+![11.0 success](https://img.shields.io/badge/11.0-success-brightgreen.svg)
+
+With this module you can easily make a schedule to backup your odoo database.
+
+Supported Cloud-Storages:
+ * Amazon WebService - S3
+ * Dropbox
+ * Google Drive
+ * ownCloud/nextCloud
+Also see: 
+
+Available Languages
+-----------
+![english](https://github.com/oxguy3/flags/raw/master/mini/gb.png)
+![german](https://github.com/oxguy3/flags/raw/master/mini/de.png)
+
+Requirements:
+---------
+Use pip to install all needed packages:
+
+```
+pip install -r requirements.txt
+```
+
+Credits
+=======
+
+Contributors
+------------
+
+
+Sponsors
+--------
+
+
+Maintainers
+--------
+* `Cytex124 <https://github.com/cytex124>`
+
+Donate
+--------
+BTC: 1Bhikgq7h6Fg6DFJ4t8HZKnPhQfNbN6vc3<br/>
+BCH: 1EGvdyqxb6wkebtxdjf6sXExY8XYpHAPTz<br/>
+ETH: 0x811d2222C78439f7589E588E950C9A28176C10D6<br/>
+LTC: LeLF1hZnRAkCUe9zXvRfqDCmjU4EUQrgvU
+
+
+Further information
+===================
+Odoo App Page: 
+
+News
+-----------
--- automatic_backup/models/models.py
+++ automatic_backup/models/models.py
@@ -2,14 +2,28 @@
 
 from odoo import models, fields, api, exceptions, service
 from enum import Enum
+
 import boto3
 import botocore
+
+import dropbox
+from dropbox import exceptions as dropbox_exceptions
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+import owncloud
+
 import os
 import datetime
 
 
+
 class BackupTypes(Enum):
     s3 = ('s3', 'Amazon Web-Service S3')
+    dropbox = ('dropbox', 'Dropbox')
+    google_drive = ('google_drive', 'Google Drive')
+    owncloud = ('owncloud', 'OwnCloud')
 
 
 class Configuration(models.Model):
@@ -19,23 +33,61 @@ class Configuration(models.Model):
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
 
+    schedule_frequently = fields.Selection(string='Every', selection=[('days', 'Days'), ('weeks', 'Weeks')],
+                                           default='weeks', required=1)
+    schedule_number = fields.Integer(min=1, default=1, required=1)
+
+    next_backup_time = fields.Datetime(default=lambda self: self._default_next_backup_time(), store=True, required=1,
+                                       help="Next Backup-Time with your UTC")
     success_mail = fields.Many2one('res.users', ondelete='set null')
     error_mail = fields.Many2one('res.users', ondelete='set null')
 
-    backup_type = fields.Selection([BackupTypes.s3.value], required=True)
+    cron_id = fields.Many2one('ir.cron', ondelete='cascade')
+
+    backup_type = fields.Selection([
+        BackupTypes.s3.value,
+        BackupTypes.dropbox.value,
+        BackupTypes.google_drive.value,
+        BackupTypes.owncloud.value
+    ], required=True)
     upload_path = fields.Char("Path to upload", required=True)
     last_backup = fields.Datetime(readonly=True)
     last_message = fields.Char(readonly=True)
     last_path = fields.Char(readonly=True)
 
     show_s3 = fields.Boolean(compute='set_show_s3', store=False)
-    s3_access_key_id = fields.Char("Access Key")
-    s3_secret_access_key = fields.Char("Secret Key")
+    show_dropbox = fields.Boolean(compute='set_show_dropbox', store=False)
+    show_owncloud = fields.Boolean(compute='set_show_owncloud', store=False)
+
+    show_access_key = fields.Boolean(compute='set_show_access_key', store=False)
+    show_secret_key = fields.Boolean(compute='set_show_secret_key', store=False)
+
+    access_key_id = fields.Char("Access Key")
+    secret_access_key = fields.Char("Secret Key")
     s3_bucket_name = fields.Char("BucketName")
+
+    cloud_url = fields.Char('URL')
+    cloud_username = fields.Char('Username')
+    cloud_password = fields.Char('Password')
 
     @api.model
     def create(self, vals):
         vals = self.check_upload_path(vals)
+        model_id = self.env['ir.model'].search([('model','=', self._name)])
+        cron_id = self.env['ir.cron'].create({
+            'name': 'Backup: '+ vals['name'],
+            'model_id': model_id.id,
+            'state': 'code',
+            'user_id': 1,
+            'interval_number': vals['schedule_number'],
+            'interval_type': vals['schedule_frequently'],
+            'nextcall': vals['next_backup_time'],
+            'priority': 100,
+            'numbercall': -1,
+            'active': vals['active'],
+            'code': 'model.action_backup('+str(self.id)+')'
+        })
+        vals['cron_id'] = cron_id.id
         result = super(Configuration, self).create(vals)
         return result
 
@@ -44,13 +96,34 @@ class Configuration(models.Model):
         self.ensure_one()
         vals = self.check_upload_path(vals)
         result = super(Configuration, self).write(vals)
+        try:
+            if self.cron_id:
+                self.cron_id.write({
+                    'interval_number': self.schedule_number,
+                    'interval_type': self.schedule_frequently,
+                    'nextcall': self.next_backup_time,
+                    'active': self.active
+                })
+                if 'name' in vals:
+                    self.cron_id.write({'name': 'Backup: '+self.name})
+        except:
+            pass
+        return result
+
+    def unlink(self):
+        cron_id = self.cron_id
+        self.cron_id = False
+        if cron_id:
+            cron_id.unlink()
+        result = super(Configuration, self).unlink()
         return result
 
     def check_upload_path(self, vals):
         if 'upload_path' in vals:
             vals['upload_path'] = vals['upload_path'].replace('\\', '/')
-            if '/' == vals['upload_path'][0]:
-                vals['upload_path'] = vals['upload_path'][1:]
+            vals['upload_path'] = ''.join(vals['upload_path'][i] for i in range(len(vals['upload_path'])-1) if (i != 0 and vals['upload_path'][i-1] != vals['upload_path'][i]) or vals['upload_path'][i] != '/')
+            if '/' != vals['upload_path'][0]:
+                vals['upload_path'] = '/' + vals['upload_path']
             if '/' != vals['upload_path'][-1]:
                 vals['upload_path'] = vals['upload_path'] + '/'
         return vals
@@ -58,9 +131,35 @@ class Configuration(models.Model):
     def set_show_s3(self):
         self.show_s3 = (self.backup_type == BackupTypes.s3.value[0])
 
+    def set_show_dropbox(self):
+        self.show_dropbox = (self.backup_type == BackupTypes.dropbox.value[0])
+
+    def set_show_owncloud(self):
+        self.show_owncloud = (self.backup_type == BackupTypes.owncloud.value[0])
+
+    def set_show_access_key(self):
+        self.show_access_key = (
+            (self.backup_type == BackupTypes.s3.value[0])
+            or (self.backup_type == BackupTypes.dropbox.value[0])
+        )
+
+    def set_show_secret_key(self):
+        self.show_secret_key = (self.backup_type == BackupTypes.s3.value[0])
+
+    def _default_next_backup_time(self):
+        return datetime.datetime.now() + datetime.timedelta(days=1)
+
     @api.onchange('backup_type')
     def onchange_backup_type(self):
         self.set_show_s3()
+        self.set_show_access_key()
+        self.set_show_secret_key()
+        self.set_show_owncloud()
+
+    def action_backup(self, id):
+        backup_ids = self.browse([id])
+        for backup in backup_ids:
+            backup.btn_action_backup()
 
     @api.multi
     def btn_action_backup(self):
@@ -68,49 +167,69 @@ class Configuration(models.Model):
         try:
             if self.backup_type == BackupTypes.s3.value[0]:
                 self._backup_on_s3()
-                self.send_email('')
+            elif self.backup_type == BackupTypes.dropbox.value[0]:
+                self._backup_on_dropbox()
+            elif self.backup_type == BackupTypes.google_drive.value[0]:
+                self._backup_on_googledrive()
+            elif self.backup_type == BackupTypes.owncloud.value[0]:
+                self._backup_on_owncloud()
+            self.send_email()
         except Exception as err:
             self.set_last_fields(err.args[0])
             self.message_post(err.args[0])
-            self.send_email(err.args[0], success=False)
+            self.send_email(success=False)
 
-    def send_email(self, message, success=True):
-        if ((success and self.success_mail.id is not False) or (not success and self.success_mail.id is not False)):
+    def send_email(self, success=True):
+        if ((success and self.success_mail.id is not False) or (not success and self.error_mail.id is not False)):
             template_name = 'backup_configuration_success_template' if success else 'backup_configuration_error_template'
             template = self.env.ref('automatic_backup.'+template_name)
             self.env['mail.template'].browse(template.id).send_mail(self.id, force_send=True)
 
     def _backup_on_s3(self):
-        if self.s3_access_key_id is False or self.s3_secret_access_key is False:
-            raise exceptions.MissingError(
-                "AWS S3: You need to add a AccessKey and a SecretAccessKey!")
+        if self.access_key_id is False or self.secret_access_key is False:
+            raise exceptions.MissingError("AWS S3: You need to add a AccessKey and a SecretAccessKey!")
         if self.s3_bucket_name is False:
-            raise exceptions.MissingError(
-                "AWS S3: You need to add a BucketName!")
+            raise exceptions.MissingError("AWS S3: You need to add a BucketName!")
 
-        client = boto3.client(
-            's3',
-            aws_access_key_id=self.s3_access_key_id,
-            aws_secret_access_key=self.s3_secret_access_key
-        )
-        filename, content = self.get_backup()
-        path = os.path.join(self.upload_path, filename)
+        client = boto3.client('s3', aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key)
+        path, content = self.get_path_and_content()
 
         try:
-            client.put_object(
-                Bucket=self.s3_bucket_name,
-                Body=content,
-                Key=path
-            )
+            client.put_object(Bucket=self.s3_bucket_name, Body=content, Key=path)
         except botocore.exceptions.ClientError as client_err:
             raise exceptions.ValidationError(
                 "AWS S3: " + client_err.response['Error']['Message'])
         else:
-            message = ("AWS S3: No Error during upload. You can "
-                      "find the backup under the bucket {0} with the "
+            message = ("AWS S3: No Error during upload. You can find the backup under the bucket {0} with the "
                       "name {1}".format(self.s3_bucket_name, path))
             self.set_last_fields(message, path=path)
             self.message_post(message)
+
+    def _backup_on_dropbox(self):
+        dbx = dropbox.Dropbox(self.access_key_id)
+        path, content = self.get_path_and_content()
+        try:
+            dbx.files_upload(content.read(), path)
+        except dropbox_exceptions.AuthError:
+            raise exceptions.ValidationError("Dropbox: Wrong Access Key!")
+        else:
+            message = ("Dropbox: No Error during upload. You can find the backup under the name {0}".format(path))
+            self.set_last_fields(message, path=path)
+            self.message_post(message)
+
+    def _backup_on_googledrive(self):
+        pass
+
+    def _backup_on_owncloud(self):
+        oc = owncloud.Client(self.cloud_url)
+        oc.login(self.cloud_username, self.cloud_password)
+        path, content = self.get_path_and_content()
+        oc.put_file_contents(path, content.read())
+
+    def get_path_and_content(self):
+        filename, content = self.get_backup()
+        path = os.path.join(self.upload_path, filename)
+        return path, content
 
     def get_backup(self, dbname=None, backup_format='zip'):
         if dbname is None:
